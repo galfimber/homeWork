b2f8ea16143db3294cbb5471cc84671d99e5c035
Nikita Varankin <galfimber2014@yandex.ru>
2023-10-31 23:06:31 +0500

    modified script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let title = "title";
+let screens = "screens";
+let screenPrice = "screenPrice";
+let rollback = "rollback";
+let fullPrice = "fullPrice";
+let adaptive = "adaptive";
+alert("Hello alert");
+console.log("Hello console");
