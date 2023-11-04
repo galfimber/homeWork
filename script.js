7f193c447188b8551ca5d9eea8f22832a88cc119
Nikita Varankin <galfimber2014@yandex.ru>
2023-11-04 14:24:40 +0500

    Выполнено задание 2

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let title = "title";
-let screens = "screens";
-let screenPrice = "screenPrice";
-let rollback = "rollback";
-let fullPrice = "fullPrice";
-let adaptive = "adaptive";
-alert("Hello alert");
-console.log("Hello console");
+let title = "Gallery";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 1000;
+let rollback = 20;
+let fullPrice = 5500;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
