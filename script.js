53565a0df04889cd9a6877275def8a7155220cad
Nikita Varankin <galfimber2014@yandex.ru>
2023-11-06 17:03:36 +0500

    Выполнено задание 3

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-let title = "Gallery";
-let screens = "Simple, Complex, Interactive";
-let screenPrice = 1000;
-let rollback = 20;
-let fullPrice = 5500;
-let adaptive = true;
+"use strict";
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const adaptive = !!prompt("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+
+console.log(adaptive);
+const rollback = 2000;
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.round(fullPrice - rollback);
+console.log(servicePercentPrice);
+// if (fullPrice >= 30000) {
+//   console.log("Даем скидку в 10%");
+// } else if (fullPrice >= 15000) {
+//   console.log("Даем скидку в 5%");
+// } else if ((fullPrice < 15000) && (0 <= fullPrice)) {
+//   console.log("Скидка не предусмотрена");
+// } else {
+//   console.log("Что то пошло не так");
+// }
+switch(fullPrice){
+    case fullPrice >= 30000:
+        console.log("Даем скидку в 10%");
+        break
+    case fullPrice >= 15000:
+        console.log("Даем скидку в 10%");
+        break
+    case fullPrice < 15000 && 0 <= fullPrice:
+        console.log("Даем скидку в 10%");
+        break
+    default:
+        console.log("Что то пошло не так");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
