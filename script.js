a5e22bb21c344163db7108a927507d65fb778271
Nikita Varankin <galfimber2014@yandex.ru>
2023-11-09 20:59:33 +0500

    Выполнено задание 3, исправления

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
-const adaptive = !!prompt("Нужен ли адаптив на сайте?");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice1 = +prompt("Сколько это будет стоить?");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
@@ -11,17 +11,17 @@ const servicePrice2 = +prompt("Сколько это будет стоить?");
 const rollback = 2000;
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.round(fullPrice - rollback);
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
 console.log(servicePercentPrice);
-switch(fullPrice){
+switch(true){
     case fullPrice >= 30000:
         console.log("Даем скидку в 10%");
         break
     case fullPrice >= 15000:
-        console.log("Даем скидку в 10%");
+        console.log("Даем скидку в 5%");
         break
     case fullPrice < 15000 && 0 <= fullPrice:
-        console.log("Даем скидку в 10%");
+        console.log("Скидка не предусмотрена");
         break
     default:
         console.log("Что то пошло не так");
