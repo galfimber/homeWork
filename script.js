5360a90a8fff78555080ca39646e4d3f42011831
Nikita Varankin <galfimber2014@yandex.ru>
2023-11-14 20:31:30 +0500

    Разрешение конфликта

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
-const title = prompt("Как называется ваш проект?");
+
+let title = prompt("Как называется ваш проект?");
+
 const screens = prompt("Какие типы экранов нужно разработать?");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
 const adaptive = confirm("Нужен ли адаптив на сайте?");
@@ -8,30 +10,51 @@ const servicePrice1 = +prompt("Сколько это будет стоить?");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt("Сколько это будет стоить?");
 
-const rollback = 2000;
+const rollback = 20;
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - rollback);
-console.log(servicePercentPrice);
-switch(true){
-    case fullPrice >= 30000:
-        console.log("Даем скидку в 10%");
-        break
-    case fullPrice >= 15000:
-        console.log("Даем скидку в 5%");
-        break
-    case fullPrice < 15000 && 0 < fullPrice:
-        console.log("Скидка не предусмотрена");
-        break
+const getRollbackMessage = function (price) {
+  switch (true) {
+    case price >= 30000:
+      return "Даем скидку в 10%";
+    case price >= 15000:
+      return "Даем скидку в 5%";
+    case price < 15000 && 0 <= price:
+      return "Скидка не предусмотрена";
     default:
-        console.log("Что то пошло не так");
+      return "Что то пошло не так";
+  }
+};
+
+const getAllServicePrices = function (servicePrice1, servicePrice2) {
+  return servicePrice1 + servicePrice2;
+};
+
+const showTypeOf = function (variable) {
+  return typeof variable;
+};
+
+function getFullPrice (screenPrice, allServicePrices) {
+  return screenPrice + allServicePrices;
+};
+
+const getTitle = function (title) {
+  title = title.trimStart().toLowerCase();
+  return title.charAt(0).toUpperCase() + title.slice(1);
+};
+
+const getServicePercentPrices = function(fullPrice, rollback){
+    return fullPrice - (fullPrice / 100 * rollback);
 }
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+title = getTitle(title);
+
+console.log(showTypeOf(title));
+console.log(showTypeOf(fullPrice));
+console.log(showTypeOf(adaptive));
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
+
