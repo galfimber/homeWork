a5bb0edb45ec6736f9ffd96956dcc6476c845870
Nikita Varankin <galfimber2014@yandex.ru>
2024-01-03 14:15:22 +0500

    Выполнено задание 26

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let title = "title";
-let screens = "screens";
-let screenPrice = "screenPrice";
-let rollback = "rollback";
-let fullPrice = "fullPrice";
-let adaptive = "adaptive";
-alert("Hello alert");
-console.log("Hello console");
+const sendUrl = "https://jsonplaceholder.typicode.com/posts";
+const getUrl = "./db.json";
+
+const sendData = (url, data) => {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((resp) => resp.json());
+};
+
+const getData = (url) => {
+  return fetch(url).then((resp) => resp.json());
+};
+
+getData(getUrl)
+  .then((data) =>
+    sendData(sendUrl, data)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+  )
+  .catch((error) => console.log(error));
